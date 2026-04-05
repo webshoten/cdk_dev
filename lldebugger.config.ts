043e0d8385a2b2c500304eac89ce5b9a17eeb7f9
webshoten <weboshimashoten@gmail.com>
2026-04-05 14:03:43 +0900

    lld設定とopennext設定

---
+++ lldebugger.config.ts
@@ -0,0 +1,8 @@
+import { type LldConfigTs } from "lambda-live-debugger";
+
+const config: LldConfigTs = {
+  framework: "cdk",
+  subfolder: "packages/infra",
+};
+
+export default config;
---
+++ packages/web/open-next.config.ts
@@ -0,0 +1,6 @@
+const config = {
+  default: {},
+  buildCommand: "npx next build",
+};
+
+export default config;
